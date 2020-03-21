95e98e03dcb6ef90c492e5e28a3ff204ddb1ac38
hungphongbk <programmingd32@gmail.com>
2020-03-21 18:12:57 +0700

    scoping

--- src/index.js
+++ src/index.js
@@ -1,36 +1,78 @@
 import incstr from 'incstr';
+import process from 'process';
+import fs from 'fs'
+
+const files = {};
+
+function sortObj(obj,fn){
+    const entries = Object.entries(obj)
+    for(let i=0;i<entries.length-1;i++) {
+        for (let j = i + 1; j < entries.length; j++) {
+            if (!fn(entries[i][1], entries[j][1])) {
+                let tmp = entries[i][1];
+                entries[i][1] = entries[j][1]
+                entries[j][1] = tmp;
+            }
+        }
+    }
+        return Object.fromEntries(entries)
+}
+function sortFiles(){
+    const fn = (a,b)=>a.c>b.c
+    for(let f in files)
+        files[f]=sortObj(files[f],fn)
+}
+
+// process.on('exit',()=>{
+//     sortFiles()
+//     console.log(files)
+// })
+
+const generateFn = ()=>incstr.idGenerator({
+    // Removed "d" letter to avoid accidental "ad" construct.
+    // @see https://medium.com/@mbrevda/just-make-sure-ad-isnt-being-used-as-a-class-name-prefix-or-you-might-suffer-the-wrath-of-the-558d65502793
+    // NOTE: allow "d" letter due to combination of UPPERCASES-lowercases
+    alphabet: 'abcefghijklmnopqrstuvwxyz0123456789_-'
+});
 
 //region CSS Scope Minify
-const createUniqueIdGenerator = () => {
+const createUniqueIdGenerator = (id) => {
     const index = {};
+    // files[id]={}
 
-    const generateNextId = incstr.idGenerator({
-        // Removed "d" letter to avoid accidental "ad" construct.
-        // @see https://medium.com/@mbrevda/just-make-sure-ad-isnt-being-used-as-a-class-name-prefix-or-you-might-suffer-the-wrath-of-the-558d65502793
-        // NOTE: allow "d" letter due to combination of UPPERCASES-lowercases
-        alphabet: 'abcdefghijklmnopqrstuvwxyz0123456789_-'
-    });
+    const generateNextId = generateFn()
 
-    return (name) => {
-        if (index[name]) {
-            return index[name];
+    return (name,scope=null) => {
+        let store=index,generate=generateNextId
+        if(scope!==null){
+            index[scope]=index[scope]||{__generate:generateFn()}
+            store=index[scope]
+            generate = index[scope].__generate
+        }
+        if (store[name]) {
+            // files[id][index[name]].c++;
+            return store[name];
         }
 
         let nextId;
 
         do {
             // Class name cannot start with a number.
-            nextId = generateNextId();
+            nextId = generate();
         } while (/^[0-9_-]/.test(nextId));
 
-        index[name] = generateNextId();
+        store[name] = nextId;
         // console.log(`${name} has id = ${index[name]}`);
+        // files[id][index[name]]={
+        //     i:name,
+        //     c:1
+        // };
 
-        return index[name];
+        return store[name];
     };
 };
 
-const idLocal = createUniqueIdGenerator(), idComponent = createUniqueIdGenerator();
+const idLocal = createUniqueIdGenerator('local'), idComponent = createUniqueIdGenerator('component');
 const components = {};
 const generateScopedName = (localName, resourcePath) => {
     const componentName = resourcePath.split('/').slice(-2).join('/');
@@ -40,7 +82,7 @@ const generateScopedName = (localName, resourcePath) => {
     }
     if (/^col-/.test(localName))
         return 'col-' + idLocal(localName);
-    return idComponent(componentName).toUpperCase() + idLocal(localName);
+    return idComponent(componentName).toUpperCase() + idLocal(localName,componentName);
 };
 
 const getLocalIdent = (context, localIdentName, localName) => generateScopedName(localName, context.resourcePath);
